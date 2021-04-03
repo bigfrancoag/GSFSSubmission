8cf789cc3eb1816c2fa87129b79efae2fc6f074a
Rene Franco <rene@rfranco.dev>
2021-04-02 19:52:39 -0500

    initial commit

---
+++ ElectronicColorCode/ElectronicColorCode.Test/OhmValuesCalculatorTest.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace ElectronicColorCode.Test
+{
+    public class OhmValuesCalculatorTest
+    {
+        [Fact]
+        public void Test()
+        {
+
+        }
+    }
+    
+}
