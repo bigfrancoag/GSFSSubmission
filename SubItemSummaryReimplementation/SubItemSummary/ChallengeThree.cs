498f8b8ffcceb48b07dd3c6e69a7d90210ca900c
Rene Franco <rene@rfranco.dev>
2021-04-02 21:55:59 -0500

    start challenge three

---
+++ SubItemSummaryReimplementation/SubItemSummary/ChallengeThree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SubItemSummary
+{
+    public class ChallengeThree
+    {
+        /// <summary>
+        /// Gets a sub-item summary for a given item number.
+        /// </summary>
+        /// <param name="itemNumber">The item number of the item to get the sub-item summary of.</param>
+        public SubItemSummary[] GetSubItemSummary(string itemNumber)
+        {
+            IEnumerable<Item> subItems = GetSubItems(itemNumber);
+
+            List<SubItemSummary> subItemSummary = new List<SubItemSummary>();
+
+            foreach (Item item in subItems)
+            {
+                IEnumerable<SubItemSummary> tempSummaries = TransformSubItems(item, item.GetSubItems());
+        
+                subItemSummary.AddRange(tempSummaries);
+            }
+
+            return subItemSummary.ToArray();
+        }
+    }
+}
